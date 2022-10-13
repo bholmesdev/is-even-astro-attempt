072bcab75038615fe5c634424dc9cd791368af8c
bholmesdev <bholmesdev@gmail.com>
2022-10-13 15:34:10 -0400

    chore: set up fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			body: ['Atkinson Hyperlegible', 'sans-serif'],
+			display: ['Chivo', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
