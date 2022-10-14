a44b4c43e2c611c306f1dd4cfd84d54ec813847b
bholmesdev <bholmesdev@gmail.com>
2022-10-14 07:51:42 -0400

    feat: add HKGrotesk

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			body: ['Atkinson Hyperlegible', 'sans-serif'],
+			body: ['HK Grotesk', 'sans-serif'],
 			display: ['Chivo', 'sans-serif'],
 		},
 		extend: {},
